acfcc243f5ee377d6ef1ce0c3a62f682df3ddcbc
tbohn2 <tanner.bohn@yahoo.com>
2023-08-18 20:59:44 -0700

    Exports all models

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Admin = require('./Admin');
+const Message = require('./Message');
+const Appt = require('./Appt');
+
+module.exports = { User, Admin, Message, Appt };
