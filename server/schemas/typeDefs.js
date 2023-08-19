eaec4c3b6aca5d64bc41c4bc72e269de0fb827e7
tbohn2 <tanner.bohn@yahoo.com>
2023-08-18 21:24:45 -0700

    Added typeDefs with queries and mutations

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,65 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        name: String!
+        address: String
+        phone: String
+        messagesFromUser: [Message]
+        apptInfo: Appt
+    }
+
+    type Admin {
+        _id: ID!
+        name: String!
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+    
+    type adminAuth {
+        token: ID!
+        admin: Admin
+    }
+
+    type Message {
+        _id: ID!
+        text: String!
+        time: String!
+        read: Boolean!
+    }
+
+    type Appt {
+        _id: ID!
+        type: String!
+        time: String!
+        servicer: Admin
+    }
+
+    type Query {
+        users: [User]
+        user(_id: ID!): User
+        admins: [Admin]
+        admin(_id: ID!): Admin
+        appts(_id: ID!): [Appt]
+    }
+
+    type Mutation {
+        addUser(name: String!, email: String!, password: String!): Auth
+        addAdmin(name: String!, password: String!): adminAuth
+        userLogin(email: String!, password: String!): Auth
+        adminLogin(name: String!, password: String!): adminAuth
+        addMessage(userId: ID!, text: String!): Message
+        addAppt(userId: ID!, type: String!, time: String!): Appt
+        assignAdmin(userId: ID!, apptId: ID!, adminId: ID): Appt
+        updateAppt(userId: ID!, apptId: ID!, type: String, time: String): Appt
+        markMessageAsRead(userId: ID!, messageId: ID!): Message
+        deleteAppt(userId: ID!, apptId: ID!): User
+        deleteMessage(userId: ID!, messageId: ID!): User
+       }
+`;
+
+module.exports = typeDefs;
