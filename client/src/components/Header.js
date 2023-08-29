24067ed4b7c86c6d50315a4bd902c9a9dcc7374b
tbohn2 <tanner.bohn@yahoo.com>
2023-08-28 20:38:24 -0700

    Fixed header return

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,23 +3,26 @@ import { Link } from 'react-router-dom';
 import Auth from '../utils/auth';
 
 const Header = () => {
-    <div>
-        <Link to='/book'>
-            <button className=''>Book Now</button>
-        </Link>
-        {Auth.loggedIn() ? (
-            <div>
-                <Link to='/appointments'>
-                    <button className='btn'>My Appointments</button>
-                </Link>
-                <button className='btn' onClick={Auth.logout}>Logout</button>
-            </div>
-        ) : (
-            <Link to='/login'>
-                <button className='btn'>Login</button>
+    return (
+        <div>
+            <Link to='/book'>
+                <button className=''>Book Now</button>
             </Link>
-        )}
-    </div>
+            {Auth.loggedIn() ? (
+                <div>
+                    <Link to='/appointments'>
+                        <button className='btn'>My Appointments</button>
+                    </Link>
+                    <button className='btn' onClick={Auth.logout}>Logout</button>
+                </div>
+            ) : (
+                <Link to='/login'>
+                    <button className='btn'>Login</button>
+                </Link>
+            )}
+        </div>
+
+    )
 };
 
 export default Header;
