4c2e7d623d9561755affbd184d7202f2dd04c9a9
tbohn2 <tanner.bohn@yahoo.com>
2023-08-28 20:19:40 -0700

    Started header

---
+++ client/src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Auth from '../utils/auth';
+
+const Header = () => {
+    <div>
+        <Link to='/book'>
+            <button className=''>Book Now</button>
+        </Link>
+        {Auth.loggedIn() ? (
+            <div>
+                <Link to='/appointments'>
+                    <button className='btn'>My Appointments</button>
+                </Link>
+                <button className='btn' onClick={Auth.logout}>Logout</button>
+            </div>
+        ) : (
+            <Link to='/login'>
+                <button className='btn'>Login</button>
+            </Link>
+        )}
+    </div>
+};
+
+export default Header;
