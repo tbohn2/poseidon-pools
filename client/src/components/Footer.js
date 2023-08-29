fb1b5c6ab94c0415f54b6b8d76f394a9664c5bea
tbohn2 <tanner.bohn@yahoo.com>
2023-08-28 20:39:09 -0700

    Created Home and Footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Auth from '../utils/auth';
+
+const Footer = () => {
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default Footer;
