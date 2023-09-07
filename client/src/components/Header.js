f16660a20acc6a1aaf605f883df19db2d5e26b85
tbohn2 <tanner.bohn@yahoo.com>
2023-09-06 17:12:29 -0700

    Fixed link buttons and started styles

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Auth from '../utils/auth';
+import './styles/header.css'
+import logo from './assets/logo.png'
 
 const Header = () => {
     return (
-        <div>
-            <Link to='/book'>
-                <button className=''>Book Now</button>
-            </Link>
-            {Auth.loggedIn() ? (
-                <div>
-                    <Link to='/appointments'>
-                        <button className='btn'>My Appointments</button>
-                    </Link>
-                    <button className='btn' onClick={Auth.logout}>Logout</button>
-                </div>
-            ) : (
-                <Link to='/login'>
-                    <button className='btn'>Login</button>
-                </Link>
-            )}
+        <div className='d-flex flex-column py-1 border-bottom '>
+            <div className='col-12 d-flex justify-content-end'>
+                <Link className='btn btn-dark m-1' to='/book'>Book Now</Link>
+                {Auth.loggedIn() ? (
+                    <div>
+                        <Link className='btn btn-dark m-1' to='/appointments'>My Appointments</Link>
+                        <button className='btn' onClick={Auth.logout}>Logout</button>
+                    </div>
+                ) : (
+                    <Link className='btn btn-dark m-1' to='/login'>Login</Link>
+                )}
+
+            </div>
+            <img className='myLogo align-self-center border col-4' src={logo}></img>
         </div>
 
     )
