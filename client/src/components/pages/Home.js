f54869218937fa7e6d1981608412c0eea3d04156
tbohn2 <tanner.bohn@yahoo.com>
2023-09-06 17:11:54 -0700

    Started home page

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import poolBG from '../assets/poolBG.jpg'
 
 const Home = () => {
     return (
-        <div className=''>
+        <div className='' style={{ backgroundImage: `url(${poolBG})` }}>
+            <p className='fs-1'>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam odit quia nesciunt exercitationem, doloribus maxime architecto. Quidem unde odio nam beatae quod reprehenderit natus fuga similique officiis, tempore incidunt placeat.
 
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam odit quia nesciunt exercitationem, doloribus maxime architecto. Quidem unde odio nam beatae quod reprehenderit natus fuga similique officiis, tempore incidunt placeat.
+
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam odit quia nesciunt exercitationem, doloribus maxime architecto. Quidem unde odio nam beatae quod reprehenderit natus fuga similique officiis, tempore incidunt placeat.
+
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam odit quia nesciunt exercitationem, doloribus maxime architecto. Quidem unde odio nam beatae quod reprehenderit natus fuga similique officiis, tempore incidunt placeat.
+            </p>
         </div>
     );
 };
