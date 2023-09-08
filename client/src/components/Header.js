74ae1763df51eb27d032fee66e4dbb18bdd5b479
tbohn2 <tanner.bohn@yahoo.com>
2023-09-07 21:08:31 -0700

    Designed header; added buttons

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,20 +6,30 @@ import logo from './assets/logo.png'
 
 const Header = () => {
     return (
-        <div className='d-flex flex-column py-1 border-bottom '>
-            <div className='col-12 d-flex justify-content-end'>
-                <Link className='btn btn-dark m-1' to='/book'>Book Now</Link>
-                {Auth.loggedIn() ? (
-                    <div>
-                        <Link className='btn btn-dark m-1' to='/appointments'>My Appointments</Link>
-                        <button className='btn' onClick={Auth.logout}>Logout</button>
-                    </div>
-                ) : (
-                    <Link className='btn btn-dark m-1' to='/login'>Login</Link>
-                )}
+        <div className='d-flex justify-content-between'>
+            <div className='d-flex justify-content-center col-5'>
+                <img className='myLogo' src={logo} alt='logo'></img>
+            </div>
+            <div className='d-flex flex-column justify-content-between align-items-center col-7'>
+                <div className='d-flex justify-content-evenly col-8 mt-5'>
+                    <p className='text-light fs-3'>Call us today!</p>
+                    <p className='text-light fs-3'>555-555-5555</p>
+                    <p className='text-light fs-3'> ppaz@ppaz.com</p>
+                </div>
+                <div className='d-flex justify-content-evenly col-12'>
+                    <Link className='btn btn-lg btn-dark' to=''>Home</Link>
+                    <Link className='btn btn-lg btn-dark' to=''>About Us</Link>
+                    <Link className='btn btn-lg btn-dark' to='/login'>View My Account</Link>
+                    {Auth.loggedIn() ? (
+                        <div>
+                            <button className='btn' onClick={Auth.logout}>Logout</button>
+                        </div>
+                    ) : (
+                        <Link className='btn btn-lg btn-dark' to='/book'>Book Now</Link>
+                    )}
+                </div>
 
             </div>
-            <img className='myLogo align-self-center border col-4' src={logo}></img>
         </div>
 
     )
