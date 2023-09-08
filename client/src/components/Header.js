31abfe4b307c9c1e644ba3b71e3d292f7dcd00d1
tbohn2 <tanner.bohn@yahoo.com>
2023-09-07 22:02:54 -0700

    Styled nav buttons

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,21 +11,22 @@ const Header = () => {
                 <img className='myLogo' src={logo} alt='logo'></img>
             </div>
             <div className='d-flex flex-column justify-content-between align-items-center col-7'>
-                <div className='d-flex justify-content-evenly col-8 mt-5'>
-                    <p className='text-light fs-3'>Call us today!</p>
-                    <p className='text-light fs-3'>555-555-5555</p>
-                    <p className='text-light fs-3'> ppaz@ppaz.com</p>
+                <div className='d-flex justify-content-evenly col-8 mt-5 fst-italic'>
+                    <p className='fs-3'>Call us today!</p>
+                    <p className='fs-3'>555-555-5555</p>
+                    <p className='fs-3'> ppaz@ppaz.com</p>
                 </div>
                 <div className='d-flex justify-content-evenly col-12'>
-                    <Link className='btn btn-lg btn-dark' to=''>Home</Link>
-                    <Link className='btn btn-lg btn-dark' to=''>About Us</Link>
-                    <Link className='btn btn-lg btn-dark' to='/login'>View My Account</Link>
+                    <Link className='btn btn-lg myNavBtn' to=''>Home</Link>
+                    <Link className='btn btn-lg myNavBtn' to='/Home#aboutUs'>About Us</Link>
+                    <Link className='btn btn-lg myNavBtn' to='/Home#ourTeam'>Meet Our Team</Link>
+                    <Link className='btn btn-lg myNavBtn' to='/login'>View My Account</Link>
                     {Auth.loggedIn() ? (
                         <div>
                             <button className='btn' onClick={Auth.logout}>Logout</button>
                         </div>
                     ) : (
-                        <Link className='btn btn-lg btn-dark' to='/book'>Book Now</Link>
+                        <Link className='btn btn-lg myNavBtn' to='/book'>Book Now</Link>
                     )}
                 </div>
 
