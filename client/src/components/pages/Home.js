28e497c59248a1cca0541cf094884fa9a253cb9a
tbohn2 <tanner.bohn@yahoo.com>
2023-09-07 22:02:12 -0700

    Changed background picture and began styling

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import poolBG from '../assets/poolBG.jpg'
+import poolFloat from '../assets/poolFloat.jpg'
+import '../styles/home.css'
 
 const Home = () => {
     return (
-        <div className='' style={{ backgroundImage: `url(${poolBG})` }}>
-            <p className='fs-1'>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam odit quia nesciunt exercitationem, doloribus maxime architecto. Quidem unde odio nam beatae quod reprehenderit natus fuga similique officiis, tempore incidunt placeat.
+        <div className='d-flex flex-column align-items-center'>
+            <div className='firstCard col-12 d-flex' style={{ backgroundImage: `url(${poolFloat})` }}>
+                <p className='fs-3 m-5 col-5'>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam odit quia nesciunt exercitationem, doloribus maxime architecto. Quidem unde odio nam beatae quod reprehenderit natus fuga similique officiis, tempore incidunt placeat.
 
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam odit quia nesciunt exercitationem, doloribus maxime architecto. Quidem unde odio nam beatae quod reprehenderit natus fuga similique officiis, tempore incidunt placeat.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam odit quia nesciunt exercitationem, doloribus maxime architecto. Quidem unde odio nam beatae quod reprehenderit natus fuga similique officiis, tempore incidunt placeat.
 
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam odit quia nesciunt exercitationem, doloribus maxime architecto. Quidem unde odio nam beatae quod reprehenderit natus fuga similique officiis, tempore incidunt placeat.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam odit quia nesciunt exercitationem, doloribus maxime architecto. Quidem unde odio nam beatae quod reprehenderit natus fuga similique officiis, tempore incidunt placeat.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam odit quia nesciunt exercitationem, doloribus maxime architecto. Quidem unde odio nam beatae quod reprehenderit natus fuga similique officiis, tempore incidunt placeat.
+                </p>
+            </div>
 
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam odit quia nesciunt exercitationem, doloribus maxime architecto. Quidem unde odio nam beatae quod reprehenderit natus fuga similique officiis, tempore incidunt placeat.
-            </p>
         </div>
     );
 };
