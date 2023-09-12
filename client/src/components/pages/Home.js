fac9dca84032740b826a3c586e00c498bcbafc04
tbohn2 <tanner.bohn@yahoo.com>
2023-09-11 19:51:12 -0700

    Added designs for buttons; created aboutUs section

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -19,7 +19,14 @@ const Home = () => {
                     <Link className='btn btn-lg mb-3 fs-3 fw-semibold bookBtn' to='/book'>Book Free Consult!</Link>
                 </div>
             </div>
-
+            <div id='aboutUs' className='col-12 mt-5 d-flex flex-column align-items-center'>
+                <h1 className='col-12 text-center fs-1 fw-bold'>About Us</h1>
+                <p className='col-10 fs-3 my-5 border border-dark'>Our mission is to be as transparent as your clean pool. We believe our clients deserve good organization and open communication. That is why we specially designed
+                    the Poseidon Pools AZ app to easily schedule when you want us to come service your pool. We serve you personally and professionally, and you can always see exactly who is coming
+                    and when. We are a family owned and operated business, and we treat our clients like family too. Join the Poseidon Pools AZ family today!
+                </p>
+                <Link className='btn btn-lg mb-3 fs-3 fw-semibold bookBtn' to='/book'>Book Free Consult!</Link>
+            </div>
         </div>
     );
 };
